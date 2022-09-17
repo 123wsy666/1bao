15b2c82711abe5bc16f1d7df2718d7ea89506469
123wsy666 <78293494+123wsy666@users.noreply.github.com>
2022-09-17 16:30:42 +0800

    添加了验证系统

--- jisuan.php
+++ jisuan.php
@@ -15,9 +15,15 @@
 	$jieguo3 = $heji - $gerenpayzong;
 	$jieguo2 = $jieguo3 / $heji;
 	$jieguobaifenshu = $jieguo2 * 100;
+	if($jieguobaifenshu > 100){
+		echo'数据不合法';
+	}elseif($jieguobaifenshu < 0){
+		echo'数据不合法';
+	}else{
 	echo $jieguobaifenshu.'%';
 	echo '<br>';
 	echo '本次医保统筹支付：'.$jieguo3.'元';
+	}
 	?>
 </center></br></h2>
 </body>
